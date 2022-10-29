b721d9f3bb1f9dfb720bef5eb7fcb5ec889c483f
PierreBeur <cbeurtheret@ucsd.edu>
2022-10-29 14:21:03 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,26 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth  = window.speechSynthesis;
+  const select = document.querySelector('#voice-select');
+  const button = document.querySelector('#explore button');
+
+  synth.onvoiceschanged = (event) => {
+    for (const voice of synth.getVoices()) {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+
+      if (voice.default) {
+        option.textContent += ' — DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      select.appendChild(option);
+    }
+  }
+
+  button.onclick = (event) => {
+    
+  }
 }
