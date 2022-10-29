2787fd4695c1fad13d000fa061ed7940f0531d48
PierreBeur <cbeurtheret@ucsd.edu>
2022-10-29 13:16:32 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,31 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.querySelector('#horn-select');
+  const img        = document.querySelector('img');
+  const audio      = document.querySelector('audio');
+  const volume     = document.querySelector('#volume');
+  const icon       = document.querySelector('#volume-controls img')
+
+  hornSelect.onchange = (event) => {
+    const value = event.target.value;
+    img.src = `assets/images/${value}.svg`;
+    audio.src = `assets/audio/${value}.mp3`;
+  };
+
+  volume.oninput = (event) => {
+    const value = event.target.value;
+    audio.volume = value / 100.0;
+
+    var level;
+         if (value == 0) { level = 0; }
+    else if (value < 33) { level = 1; }
+    else if (value < 67) { level = 2; }
+    else                 { level = 3; }
+
+    icon.src = `assets/icons/volume-level-${level}.svg`;
+    icon.alt = `Volume level ${level}`;
+  };
+
+  
 }
